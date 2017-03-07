ef78667a4eea4dc8f54bae605a8dbd4615c37b5d
pdbq21 <pdbq2122@gmail.com>
2017-03-07 18:06:14 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import App from './App';
 import './index.css';
 
+import configureStore from './app/store/configureStore';
+
+const store = configureStore();
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
+/*
+ ReactDOM.render(
+ <BuilderContainer />,
+ document.getElementById('root-app-pb')
+ );*/
