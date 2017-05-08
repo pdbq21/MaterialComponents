9d25cab4508cfad5b9927c7b56df55a40edd8a94
pdbq21 <pdbq2122@gmail.com>
2017-05-08 18:05:54 +0300

    done: Ripple 99%

--- src/app/lib/Ripple/Ripple.js
+++ src/app/lib/Ripple/Ripple.js
@@ -3,6 +3,7 @@
  */
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import {ripple as test}  from 'material-components-web/dist/material-components-web';
 const {MDCRippleFoundation} = test;
 
@@ -40,8 +41,8 @@ export default class Ripple extends PureComponent {
         this.foundation = null;
     }
 
-    foundation_ = () => (new MDCRippleFoundation({
-        isUnbounded: () => false,
+    foundation_ = () => new MDCRippleFoundation({
+        isUnbounded: () => (this.props.unbounded),
         browserSupportsCssVars: () => {
             return supportsCssVariables(window);
         },
@@ -79,19 +80,40 @@ export default class Ripple extends PureComponent {
         },
         computeBoundingRect: () => {
             if (this.refs.root) {
-                return this.refs.root.getBoundingClientRect()
+                if (this.props.unbounded) {
+                    const {left, top} = this.refs.root.getBoundingClientRect();
+                    const DIM = this.props.diameter || 40;
+                    return {
+                        top,
+                        left,
+                        right: left + DIM,
+                        bottom: top + DIM,
+                        width: DIM,
+                        height: DIM,
+                    };
+                } else {
+                    return this.refs.root.getBoundingClientRect();
+                }
             }
         },
         getWindowPageOffset: () => ({
             x: window.pageXOffset,
             y: window.pageYOffset
         }),
-    }));
+    });
 
     render() {
+        const {children, unbounded, primary, accent} = this.props;
+        const isUnbounded = (unbounded)? true : null;
+        const classes = classnames('mdc-ripple-surface', {
+            'mdc-ripple-surface--primary': primary,
+            'mdc-ripple-surface--accent': accent
+        }, children.props.className);
         return (
-            React.cloneElement(this.props.children, {
-                ref: 'root'
+            React.cloneElement(children, {
+                ref: 'root',
+                className: classes,
+                ['data-mdc-ripple-is-unbounded']: isUnbounded
             })
         );
     }
@@ -106,133 +128,133 @@ export default class Ripple extends PureComponent {
     }
 }
 /*
-function getMatchesProperty(HTMLElementPrototype) {
-    return [
-        'webkitMatchesSelector', 'msMatchesSelector', 'matches',
-    ].filter((p) => p in HTMLElementPrototype).pop();
-}
+ function getMatchesProperty(HTMLElementPrototype) {
+ return [
+ 'webkitMatchesSelector', 'msMatchesSelector', 'matches',
+ ].filter((p) => p in HTMLElementPrototype).pop();
+ }
 
-const MATCHES = getMatchesProperty(HTMLElement.prototype);
+ const MATCHES = getMatchesProperty(HTMLElement.prototype);
 
-function supportsCssVariables(windowObj) {
-    const supportsFunctionPresent = windowObj.CSS && typeof windowObj.CSS.supports === "function";
-    if (!supportsFunctionPresent) {
-        return false;
-    }
+ function supportsCssVariables(windowObj) {
+ const supportsFunctionPresent = windowObj.CSS && typeof windowObj.CSS.supports === "function";
+ if (!supportsFunctionPresent) {
+ return false;
+ }
 
-    const explicitlySupportsCssVars = windowObj.CSS.supports("--css-vars", "yes");
-    // See: https://bugs.webkit.org/show_bug.cgi?id=154669
-    // See: README section on Safari
-    const weAreFeatureDetectingSafari10plus = (
-        windowObj.CSS.supports("(--css-vars: yes)") &&
-        windowObj.CSS.supports("color", "#00000000")
-    );
-    return explicitlySupportsCssVars || weAreFeatureDetectingSafari10plus;
-}
+ const explicitlySupportsCssVars = windowObj.CSS.supports("--css-vars", "yes");
+ // See: https://bugs.webkit.org/show_bug.cgi?id=154669
+ // See: README section on Safari
+ const weAreFeatureDetectingSafari10plus = (
+ windowObj.CSS.supports("(--css-vars: yes)") &&
+ windowObj.CSS.supports("color", "#00000000")
+ );
+ return explicitlySupportsCssVars || weAreFeatureDetectingSafari10plus;
+ }
 
-export default class extends Component {
-    static propTypes = {
-        id: PropTypes.string,
-    }
-
-
-    state = {
-        classNames: [],
-        rippleCss: {},
-    }
+ export default class extends Component {
+ static propTypes = {
+ id: PropTypes.string,
+ }
 
 
-    foundation = new MDCRippleFoundation(Object.assign(MDCRipple.createAdapter(this), {
-        // for FAB this. === true \ for other component === false
-        isUnbounded: () => false,
-        browserSupportsCssVars: () => {
-            return supportsCssVariables(window);
-        },
-        isSurfaceActive: () => this.refs.root[MATCHES](':active'),
-        addClass: className => this.setState(({classNames}) => ({
-            classNames: classNames.concat([className])
-        })),
-        removeClass: className => this.setState(({classNames}) => ({
-            classNames: classNames.filter(cn => cn !== className)
-        })),
-        registerInteractionHandler: (evtType, handler) => {
-            this.refs.root.addEventListener(evtType, handler);
-        },
-        deregisterInteractionHandler: (evtType, handler) => {
-            this.refs.root.removeEventListener(evtType, handler);
-        },
-        registerResizeHandler: handler => {
-            window.addEventListener('resize', handler);
-        },
-        deregisterResizeHandler: handler => {
-            window.removeEventListener('resize', handler);
-        },
+ state = {
+ classNames: [],
+ rippleCss: {},
+ }
 
 
-        updateCssVariable: (varName, value) => this.setState(({rippleCss}) => ({
-            rippleCss: {
-                ...rippleCss,
-                [varName]: value
-            }
-        })),
-        computeBoundingRect: () => {
-            //console.log(this.refs.root.getBoundingClientRect());
-
-            /!*const {left, top} = this.refs.root.getBoundingClientRect();
-            console.log(left, top);
-            const DIM = 40;*!/
-            return this.refs.root.getBoundingClientRect();
-            /!*return {
-                top,
-                left,
-                right: left + DIM,
-                bottom: top + DIM,
-                width: DIM,
-                height: DIM,
-            };*!/
-        },
-        getWindowPageOffset: () => {
-            return {
-                x: window.pageXOffset,
-                y: window.pageYOffset
-            }
-        },
-
-    }));
+ foundation = new MDCRippleFoundation(Object.assign(MDCRipple.createAdapter(this), {
+ // for FAB this. === true \ for other component === false
+ isUnbounded: () => false,
+ browserSupportsCssVars: () => {
+ return supportsCssVariables(window);
+ },
+ isSurfaceActive: () => this.refs.root[MATCHES](':active'),
+ addClass: className => this.setState(({classNames}) => ({
+ classNames: classNames.concat([className])
+ })),
+ removeClass: className => this.setState(({classNames}) => ({
+ classNames: classNames.filter(cn => cn !== className)
+ })),
+ registerInteractionHandler: (evtType, handler) => {
+ this.refs.root.addEventListener(evtType, handler);
+ },
+ deregisterInteractionHandler: (evtType, handler) => {
+ this.refs.root.removeEventListener(evtType, handler);
+ },
+ registerResizeHandler: handler => {
+ window.addEventListener('resize', handler);
+ },
+ deregisterResizeHandler: handler => {
+ window.removeEventListener('resize', handler);
+ },
 
 
-    render() {
-        return (
-            <div
-                ref="root"
-                className={
-                    classnames('material-icons','mdc-ripple-surface', this.state.classNames)
-                }
-                aria-label="Favorite"
-                data-mdc-ripple-is-unbounded
-                tabIndex="0"
-            >
+ updateCssVariable: (varName, value) => this.setState(({rippleCss}) => ({
+ rippleCss: {
+ ...rippleCss,
+ [varName]: value
+ }
+ })),
+ computeBoundingRect: () => {
+ //console.log(this.refs.root.getBoundingClientRect());
 
-            </div>
-        );
-    }
+ /!*const {left, top} = this.refs.root.getBoundingClientRect();
+ console.log(left, top);
+ const DIM = 40;*!/
+ return this.refs.root.getBoundingClientRect();
+ /!*return {
+ top,
+ left,
+ right: left + DIM,
+ bottom: top + DIM,
+ width: DIM,
+ height: DIM,
+ };*!/
+ },
+ getWindowPageOffset: () => {
+ return {
+ x: window.pageXOffset,
+ y: window.pageYOffset
+ }
+ },
 
-    componentDidMount() {
-        this.foundation.init();
-    }
+ }));
 
-    componentWillUnmount() {
-        this.foundation.destroy();
-    }
 
-    componentDidUpdate() {
-        if (this.refs.root) {
-            for (let key in this.state.rippleCss) {
-                if (this.state.rippleCss.hasOwnProperty(key)) {
-                    this.refs.root.style.setProperty(key, this.state.rippleCss[key]);
-                }
-            }
-        }
-    }
-}
-*/
+ render() {
+ return (
+ <div
+ ref="root"
+ className={
+ classnames('material-icons','mdc-ripple-surface', this.state.classNames)
+ }
+ aria-label="Favorite"
+ data-mdc-ripple-is-unbounded
+ tabIndex="0"
+ >
+
+ </div>
+ );
+ }
+
+ componentDidMount() {
+ this.foundation.init();
+ }
+
+ componentWillUnmount() {
+ this.foundation.destroy();
+ }
+
+ componentDidUpdate() {
+ if (this.refs.root) {
+ for (let key in this.state.rippleCss) {
+ if (this.state.rippleCss.hasOwnProperty(key)) {
+ this.refs.root.style.setProperty(key, this.state.rippleCss[key]);
+ }
+ }
+ }
+ }
+ }
+ */
--- src/demo/components/RippleComponent.js
+++ src/demo/components/RippleComponent.js
@@ -16,8 +16,10 @@ export default class RippleComponent extends React.PureComponent {
                 <fieldset>
                     <legend>Ripple Component</legend>
                     <div>
+                        <h2>Bounded</h2>
                         <Ripple>
-                            <div className="mdc-ripple-surface mdc-elevation--z2"
+
+                            <div className="mdc-elevation--z2"
                                  tabIndex="0"
                                  style={{
                                      'height': '10em',
@@ -28,7 +30,32 @@ export default class RippleComponent extends React.PureComponent {
                             </div>
                         </Ripple>
                     </div>
-
+<h2>Unbounded</h2>
+                    <div>
+                        <Ripple
+                            unbounded
+                        >
+                            <div className="material-icons demo-surface"
+                                 aria-label="Favorite"
+                                 tabIndex="0"
+                            >
+                                favorite
+                            </div>
+                        </Ripple>
+                    </div>
+                    <h2>Theme Styles</h2>
+                    <div>
+                        <Ripple
+                            unbounded
+                        >
+                            <div className="demo-surface material-icons"
+                                 tabIndex="0"
+                                 aria-label="Favorite"
+                            >
+                                favorite
+                            </div>
+                        </Ripple>
+                    </div>
                 </fieldset>
                 <fieldset>
                     <legend>Only CSS</legend>
