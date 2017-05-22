fcc86b7da4b24a16ae419c526fb41869ff6b1c21
pdbq21 <pdbq2122@gmail.com>
2017-05-22 11:54:45 +0300

    need: upgrade material-components-web

--- src/demo/containers/ComponentsView.js
+++ src/demo/containers/ComponentsView.js
@@ -120,7 +120,50 @@ export default class App extends Component{
     render(){
         return(
             <div>
-                test App
+                <Toolbar
+                    onChange={this.handleChange}
+                    fixed
+                >
+                    <ToolbarRow>
+                        <ToolbarSection start>
+                            <a className="material-icons">menu</a>
+                            <ToolbarTitle>Title</ToolbarTitle>
+                        </ToolbarSection>
+                        <ToolbarSection end>
+                            <a className="material-icons" aria-label="Download" alt="Download">file_download</a>
+                            <a className="material-icons" aria-label="Print this page"
+                               alt="Print this page">print</a>
+                            <a className="material-icons" aria-label="Bookmark this page" alt="Bookmark this page">bookmark</a>
+                        </ToolbarSection>
+                    </ToolbarRow>
+                </Toolbar>
+                <ToolbarMain fixed>
+                    <h3>Is Top</h3>
+                    <p>
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                        egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+                        Donec
+                        eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Pellentesque
+                        habitant
+                        morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+                        tortor
+                        quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet
+                        quam
+                        egestas semper. Aenean ultricies mi vitae est.
+                    </p>
+                    <p>
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                        egestas.
+                        Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu
+                        libero
+                        sit amet quam egestas semper. Aenean ultricies mi vitae est. Pellentesque habitant morbi
+                        tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor
+                        quam,
+                        feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+                        egestas
+                        semper. Aenean ultricies mi vitae est.
+                    </p>
+                </ToolbarMain>
             </div>
         )
     }
