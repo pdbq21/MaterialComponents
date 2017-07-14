776d7f3c7f397f329330430b4ee2ad883fadf3c6
pdbq21 <pdbq2122@gmail.com>
2017-07-14 12:32:16 +0300

    test: travis

--- src/react-material-components-demo/pages/Home.js
+++ src/react-material-components-demo/pages/Home.js
@@ -2,16 +2,23 @@
  * Created by ruslan on 05.06.17.
  */
 import React, { Component } from 'react'
-
+function getCurrentTime(callback) {
+// Get the current 'global' time from an API
+  return setTimeout(function() {
+    var currentTime = new Date();
+    callback(currentTime);
+  }, 2000);
+}
+getCurrentTime(function(currentTime) {
+  console.log('The current time is: ' + currentTime);
+});
 export default class Home extends Component {
 
     render() {
         return (
             <div >
 home
-                Todo list:
-                - add basic structure for all Components
-                
+
             </div>
         )
     }
